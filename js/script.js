89a2d55cd96802a8b6ad7b338717409f3527088a
Damjan15 <stamenovdamjan5@gmail.com>
2025-03-22 18:53:15 +0100

    feat: mobile menu & hamburger menu functionality added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Welcome to Tutor!");
+document.addEventListener("DOMContentLoaded", function () {
+  // Get hamburger button and mobile menu items
+  const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
+  const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
+
+  // Add click event listener to toggle menu visibility
+  toggleButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
+  });
+});
